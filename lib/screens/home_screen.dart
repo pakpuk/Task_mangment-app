b460b94e78f035dcffe54ee00b13dc0bc3443d7a
Ghaith <ghayth.1900@gmail.com>
2025-09-25 17:35:45 +0200

    working on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,9 +12,25 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16.0),
           child: Column(
-            children: [Row()],
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: ClipRRect(
+                      child: Image.asset(
+                          'assets/images/jakob-owens-lkMJcGDZLVs-unsplash.jpg'),
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
