4be26a25b9dbb780956013e3a3867beddb315ef5
Ghaith <ghayth.1900@gmail.com>
2025-09-25 14:31:50 +0200

    add appcolor file

---
+++ lib/core/appcolor.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Appcolor {
+  static const Color kYellewLight = Color(0XFFFFF7EC);
+  static const Color kkYellew = Color(0XFFFAF0DA);
+  static const Color kYellewDark = Color(0xFFEBBB7F);
+  static const Color kRedLight = Color(0xFFFCF0F0F);
+  static const Color kRed = Color(0xFFFBE4E6);
+  static const Color kRedDark = Color(0XFFF08A8E);
+  static const Color kBlueLight = Color(0XFFEDF4FE);
+  static const Color kBlue = Color(0XFFE1EDFC);
+  static const Color kBlueDark = Color(0XFFC0D3F8);
+}
