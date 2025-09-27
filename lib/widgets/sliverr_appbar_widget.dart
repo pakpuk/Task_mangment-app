f36c5bf3e0ec02d8b55793dfe20c80785b1b3a47
Ghaith <ghayth.1900@gmail.com>
2025-09-27 20:50:31 +0200

    worrking  on logic of the  app

--- lib/widgets/sliverr_appbar_widget.dart
+++ lib/widgets/sliverr_appbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanagment_app/core/appcolor.dart';
 
 class SliverrAppbarWidget extends StatelessWidget {
   const SliverrAppbarWidget({super.key});
@@ -7,6 +8,20 @@ class SliverrAppbarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       expandedHeight: 90,
+      leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Appcolor.kwhite,
+            size: 20,
+          )),
+      actions: [
+        Icon(
+          Icons.more_vert,
+          size: 30,
+          color: Appcolor.kwhite,
+        )
+      ],
     );
   }
 }
