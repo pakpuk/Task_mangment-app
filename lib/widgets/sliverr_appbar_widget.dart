756bb8bf428008f0953207d8d7eca66b0f8b3961
Ghaith <ghayth.1900@gmail.com>
2025-09-27 20:58:52 +0200

    worrking  on logic of the  app

--- lib/widgets/sliverr_appbar_widget.dart
+++ lib/widgets/sliverr_appbar_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:taskmanagment_app/core/appcolor.dart';
+import 'package:taskmanagment_app/models/Task_model.dart';
 
 class SliverrAppbarWidget extends StatelessWidget {
-  const SliverrAppbarWidget({super.key});
-
+  const SliverrAppbarWidget(
+      {super.key, required this.title, required this.left});
+  final String title;
+  final num left;
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
@@ -20,6 +23,26 @@ class SliverrAppbarWidget extends StatelessWidget {
           Icons.more_vert,
           size: 30,
           color: Appcolor.kwhite,
+        ),
+        FlexibleSpaceBar(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${title} Tasks",
+                style: TextStyle(
+                    color: Appcolor.kwhite,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "You havve ${left} tasks for today!",
+                style: TextStyle(color: Appcolor.kgrey, fontSize: 12),
+              )
+            ],
+          ),
         )
       ],
     );
