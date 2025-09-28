58a78f82571347751ad7932b1eebead04b1c6a1c
Ghaith <ghayth.1900@gmail.com>
2025-09-28 21:04:55 +0200

    finish datepickerr widget

---
+++ lib/widgets/task_title_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskTitleWidget extends StatelessWidget {
+  const TaskTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [],
+    );
+  }
+}
