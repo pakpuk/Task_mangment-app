6c3c565fac63c4db69240d07aa40b8dda830a65c
Ghaith <ghayth.1900@gmail.com>
2025-09-28 18:03:39 +0200

    working  or  task detail screen

--- lib/widgets/date_picker.dart
+++ lib/widgets/date_picker.dart
@@ -10,12 +10,33 @@ class DatePicker extends StatefulWidget {
 class _DatePickerState extends State<DatePicker> {
   final weekList = [24, 25, 26, 27, 28, 29, 30];
   final dayList = ["Sun", "Mon", "Thu", "Wed", "Tue", "Fri", "Sut"];
-  var isSelected=4;
+  var isSelected = 4;
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(),
-      child: ,
+      height: 100,
+      padding: EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      )),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+          itemBuilder: (context,index){
+            return GestureDetector(
+              onTap: (){},
+              child: Container(
+                child: Column(
+                  children: [],
+                ),
+              ),
+            )
+          },
+          separatorBuilder:(_ ,SizedBox){
+
+          } ,
+          itemCount: weekList.length),
     );
   }
 }
